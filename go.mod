e8eb44224f9a96c0dad2598f28ee893c0de1d165
Peter Loftus <loftusp5976@gmail.com>
2023-09-13 13:05:40 -0400

    Inital .env setup

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/pedro-loftus/rss-aggregator
 
 go 1.21.1
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	godotenv.Load()
+
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT is not found in environment")
+	}
+
+	fmt.Println("Port:", portString)
 }
